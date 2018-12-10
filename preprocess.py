4d0c66bc95e67e7af586580d5c605ad761c9ec91
AllanSCosta <allanc@mit.edu>
2018-12-10 11:39:02 +0100

    transform to GAN

--- preprocess.py
+++ preprocess.py
@@ -3,41 +3,35 @@ import numpy as np
 import os
 import pyworld
 
-def load_wavs(wav_dir, sr):
 
-    wavs = list()
+def load_wavs(wav_dir, sr):
+    wavs = []
     for file in os.listdir(wav_dir):
         file_path = os.path.join(wav_dir, file)
-        wav, _ = librosa.load(file_path, sr = sr, mono = True)
-        #wav = wav.astype(np.float64)
+        wav, _ = librosa.load(file_path, sr=sr, mono=True)
         wavs.append(wav)
-
     return wavs
 
-def world_decompose(wav, fs, frame_period = 5.0):
 
+def world_decompose(wav, fs, frame_period = 5.0):
     # Decompose speech signal into f0, spectral envelope and aperiodicity using WORLD
     wav = wav.astype(np.float64)
-    f0, timeaxis = pyworld.harvest(wav, fs, frame_period = frame_period, f0_floor = 71.0, f0_ceil = 800.0)
+    f0, timeaxis = pyworld.harvest(wav, fs, frame_period=frame_period, f0_floor=71.0, f0_ceil=800.0)
     sp = pyworld.cheaptrick(wav, f0, timeaxis, fs)
     ap = pyworld.d4c(wav, f0, timeaxis, fs)
 
     return f0, timeaxis, sp, ap
 
+
 def world_encode_spectral_envelop(sp, fs, dim = 24):
-
     # Get Mel-cepstral coefficients (MCEPs)
-
-    #sp = sp.astype(np.float64)
     coded_sp = pyworld.code_spectral_envelope(sp, fs, dim)
 
     return coded_sp
 
-def world_decode_spectral_envelop(coded_sp, fs):
 
+def world_decode_spectral_envelop(coded_sp, fs):
     fftlen = pyworld.get_cheaptrick_fft_size(fs)
-    #coded_sp = coded_sp.astype(np.float32)
-    #coded_sp = np.ascontiguousarray(coded_sp)
     decoded_sp = pyworld.decode_spectral_envelope(coded_sp, fs, fftlen)
 
     return decoded_sp
@@ -72,8 +66,7 @@ def transpose_in_list(lst):
 
 
 def world_decode_data(coded_sps, fs):
-
-    decoded_sps =  list()
+    decoded_sps = list()
 
     for coded_sp in coded_sps:
         decoded_sp = world_decode_spectral_envelop(coded_sp, fs)
@@ -83,8 +76,7 @@ def world_decode_data(coded_sps, fs):
 
 
 def world_speech_synthesis(f0, decoded_sp, ap, fs, frame_period):
-
-    #decoded_sp = decoded_sp.astype(np.float64)
+    # decoded_sp = decoded_sp.astype(np.float64)
     wav = pyworld.synthesize(f0, decoded_sp, ap, fs, frame_period)
     # Librosa could not save wav if not doing so
     wav = wav.astype(np.float32)
@@ -112,16 +104,18 @@ def coded_sps_normalization_fit_transoform(coded_sps):
     coded_sps_normalized = list()
     for coded_sp in coded_sps:
         coded_sps_normalized.append((coded_sp - coded_sps_mean) / coded_sps_std)
-    
+
     return coded_sps_normalized, coded_sps_mean, coded_sps_std
+
 
 def coded_sps_normalization_transoform(coded_sps, coded_sps_mean, coded_sps_std):
 
     coded_sps_normalized = list()
     for coded_sp in coded_sps:
         coded_sps_normalized.append((coded_sp - coded_sps_mean) / coded_sps_std)
-    
+
     return coded_sps_normalized
+
 
 def coded_sps_normalization_inverse_transoform(normalized_coded_sps, coded_sps_mean, coded_sps_std):
 
@@ -130,6 +124,7 @@ def coded_sps_normalization_inverse_transoform(normalized_coded_sps, coded_sps_m
         coded_sps.append(normalized_coded_sp * coded_sps_std + coded_sps_mean)
 
     return coded_sps
+
 
 def coded_sp_padding(coded_sp, multiple = 4):
 
@@ -143,15 +138,16 @@ def coded_sp_padding(coded_sp, multiple = 4):
 
     return coded_sp_padded
 
+
 def wav_padding(wav, sr, frame_period, multiple = 4):
 
-    assert wav.ndim == 1 
+    assert wav.ndim == 1
     num_frames = len(wav)
     num_frames_padded = int((np.ceil((np.floor(num_frames / (sr * frame_period / 1000)) + 1) / multiple + 1) * multiple - 1) * (sr * frame_period / 1000))
     num_frames_diff = num_frames_padded - num_frames
     num_pad_left = num_frames_diff // 2
     num_pad_right = num_frames_diff - num_pad_left
-    wav_padded = np.pad(wav, (num_pad_left, num_pad_right), 'constant', constant_values = 0)
+    wav_padded = np.pad(wav, (num_pad_left, num_pad_right), 'constant', constant_values=0)
 
     return wav_padded
 
@@ -164,12 +160,13 @@ def logf0_statistics(f0s):
 
     return log_f0s_mean, log_f0s_std
 
-def pitch_conversion(f0, mean_log_src, std_log_src, mean_log_target, std_log_target):
 
+def pitch_conversion(f0, mean_log_src, std_log_src, mean_log_target, std_log_target):
     # Logarithm Gaussian normalization for Pitch Conversions
     f0_converted = np.exp((np.log(f0) - mean_log_src) / std_log_src * std_log_target + mean_log_target)
 
     return f0_converted
+
 
 def wavs_to_specs(wavs, n_fft = 1024, hop_length = None):
 
@@ -200,8 +197,28 @@ def mfccs_normalization(mfccs):
     mfccs_normalized = list()
     for mfcc in mfccs:
         mfccs_normalized.append((mfcc - mfccs_mean) / mfccs_std)
-    
+
     return mfccs_normalized, mfccs_mean, mfccs_std
+
+
+def sample_single_train_data(dataset, n_frames = 128):
+    num_samples = len(dataset)
+    train_data_idx = np.arange(len(dataset))
+    np.random.shuffle(train_data_idx)
+    train_data_idx_subset = train_data_idx[:num_samples]
+    train_data = []
+
+    for idx in train_data_idx_subset:
+        data = dataset[idx]
+        frames_total = data.shape[1]
+        # TODO(ALLAN): Fix THIS
+        if frames_total < n_frames:
+            continue
+        start = np.random.randint(frames_total - n_frames + 1)
+        end = start + n_frames
+        train_data.append(data[:, start:end])
+
+    return np.array(train_data)
 
 
 def sample_train_data(dataset_A, dataset_B, n_frames = 128):
